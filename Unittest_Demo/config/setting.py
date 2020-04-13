208ba4faf955d48befc974ff77c7553defd02dce
王琴 <wq48959@ly.com>
2020-04-13 17:27:16 +0800

    Update setting.py
    111

--- Unittest_Demo/config/setting.py
+++ Unittest_Demo/config/setting.py
@@ -5,11 +5,11 @@
 # @author：wongqin
 
 mail_date = {
-    'addr':'wq48959@ly.com',            # 邮件收件人,
-    'addr2':'1460538304@qq.com',        # 邮件收件人,
-    'cc':'wq48959@ly.com',              # 邮件抄送人
-    'sender':'1460538304@qq.com',       # 邮件的发送者，主要用于第三方邮件发送
-    'code':'pctendspjcgmhjfh',          # 邮件的授权码，主要用于第三方邮件发送
+    'addr':'xxx',            # 邮件收件人,
+    'addr2':'xxx',        # 邮件收件人,
+    'cc':'xxx',              # 邮件抄送人
+    'sender':'xxx',       # 邮件的发送者，主要用于第三方邮件发送
+    'code':'xxxx',          # 邮件的授权码，主要用于第三方邮件发送
     'port':465,                         # 端口
     'server':'smtp.qq.com',             # 邮箱的服务名
 }
